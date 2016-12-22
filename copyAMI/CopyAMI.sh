a0b4c43468bb8857354ef2e17dd00aa03fdafb0d
Noel Georgi <noel.georgi@reancloud.com>
2016-12-22 21:18:16 +0530

    Exposed region also as a variable

--- copyAMI/CopyAMI.sh
+++ copyAMI/CopyAMI.sh
@@ -2,10 +2,12 @@
 GREEN='\033[0;32m' #Green color
 NC='\033[0m' #  No Color
 BLUE='\033[0;34m' #Blue color
+
+REGIONS=* # if set to *, looks up in all region, otherwise provide a comma separated list of regions without space. Eg: us-east-1,eu-west-1
 SOURCE_REGION=us-east-1 #The region where AMI is present
 PROFILE_NAME=default #AWS profile name, default value is default
 AMI_NAMES=MyAMI1,MyAMI2 #Provide the AMI names as comma separted values
-aws_regions=($(aws ec2 describe-regions --filters "Name=region-name,Values=*" | jq -r '.Regions[] .RegionName'))
+aws_regions=($(aws ec2 describe-regions --filters "Name=region-name,Values=$REGIONS" | jq -r '.Regions[] .RegionName'))
 existing_ami_name=($(aws ec2 describe-images --profile $PROFILE_NAME --filter "Name=name,Values=$AMI_NAMES" --region $SOURCE_REGION | jq -r '.Images[] .Name'))
 existing_ami_id=($(aws ec2 describe-images --profile $PROFILE_NAME --filter "Name=name,Values=$AMI_NAMES" --region $SOURCE_REGION | jq -r '.Images[] .ImageId'))
 for region in ${aws_regions[@]}
