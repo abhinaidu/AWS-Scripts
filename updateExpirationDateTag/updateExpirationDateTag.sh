8f1187ecb1be7307dd9e205a141329c846833eb6
Noel Georgi <noel.georgi@reancloud.com>
2016-12-24 15:33:30 +0530

    Added days untill expiry variable

--- updateExpirationDateTag/updateExpirationDateTag.sh
+++ updateExpirationDateTag/updateExpirationDateTag.sh
@@ -2,14 +2,17 @@
 set -e
 # Variables 
 PROFILE=default # set the AWS profile name, defaults to default
-REGIONS=* # if set to *, looks up in all region, otherwise provide a comma separated list of regions without space. Eg: us-east-1,eu-west-1
+REGIONS='*' # if set to *, looks up in all region, otherwise provide a comma separated list of regions without space. Eg: us-east-1,eu-west-1
 OWNER_NAME=john.doe # please provide your owner name
 DAYS_TO_UPDATE=5 # the number of days forward to update the ExpirationDate tag to.
+DAYS_TO_EXPIRY=3 # the number of days untill expiry
+
 GREEN='\033[0;32m' #Green color
 NC='\033[0m' #  No Color
 BLUE='\033[0;34m' #Blue color
 RED='\033[0;31m' #Red color
 BROWN='\033[0;33m' #Purple color
+
 aws_regions=($(aws ec2 describe-regions --filters "Name=region-name,Values=$REGIONS" | jq -r '.Regions[] .RegionName'))
 current_time=$(date +%s) #converting the current date to UNIX epoch format
 for region in ${aws_regions[@]}
@@ -23,7 +26,7 @@ do
 		#get the instance name
 		instance_name=$(aws ec2 describe-tags --filters "Name=resource-id, Values=$instance_id" --region $region --profile $PROFILE | jq -r '.Tags[] | select(.Key == "Name") .Value')
 		#Checking whetehr the date is nearing expiration
-		if [ "$current_expiration_time" -le "$current_time" ]; then
+		if [ "$(((current_expiration_time-current_time)/86400))" -le "$DAYS_TO_EXPIRY" ]; then
 			#Deleting the current ExpirationDate tag
 			aws ec2 delete-tags --resource $instance_id --tags Key='ExpirationDate' --region $region --profile $PROFILE
 			#Calculating the new expiration date
