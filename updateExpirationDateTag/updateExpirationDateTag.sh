473ef9f07102bfedcbac1979098ee5a0b5c0d30d
Noel Georgi <noel.georgi@reancloud.com>
2016-12-24 15:48:16 +0530

    Added missing quotes

--- updateExpirationDateTag/updateExpirationDateTag.sh
+++ updateExpirationDateTag/updateExpirationDateTag.sh
@@ -15,25 +15,25 @@ BROWN='\033[0;33m' #Purple color
 
 aws_regions=($(aws ec2 describe-regions --filters "Name=region-name,Values=$REGIONS" | jq -r '.Regions[] .RegionName'))
 current_time=$(date +%s) #converting the current date to UNIX epoch format
-for region in ${aws_regions[@]}
+for region in "${aws_regions[@]}"
 do
 	#Get the Instance id
 	instance_ids=($(aws ec2 describe-instances --filter "Name=tag-value,Values=$OWNER_NAME" --region "$region" --profile $PROFILE | jq -r '.Reservations[] .Instances[] .InstanceId'))
-	for instance_id in ${instance_ids[@]}
+	for instance_id in "${instance_ids[@]}"
 	do
 		#Get the expiration date for the instance in UNIX epoch format
-		current_expiration_time=$(date -d $(aws ec2 describe-tags --filters "Name=resource-id, Values=$instance_id" --region $region --profile $PROFILE | jq -r '.Tags[] | select(.Key =="ExpirationDate") .Value') +%s)
+		current_expiration_time=$(date -d "$(aws ec2 describe-tags --filters "Name=resource-id, Values=$instance_id" --region "$region" --profile "$PROFILE" | jq -r '.Tags[] | select(.Key =="ExpirationDate") .Value')" +%s)
 		#get the instance name
-		instance_name=$(aws ec2 describe-tags --filters "Name=resource-id, Values=$instance_id" --region $region --profile $PROFILE | jq -r '.Tags[] | select(.Key == "Name") .Value')
+		instance_name=$(aws ec2 describe-tags --filters "Name=resource-id, Values=$instance_id" --region "$region" --profile $PROFILE | jq -r '.Tags[] | select(.Key == "Name") .Value')
 		#Checking whetehr the date is nearing expiration
 		if [ "$(((current_expiration_time-current_time)/86400))" -le "$DAYS_TO_EXPIRY" ]; then
 			#Deleting the current ExpirationDate tag
-			aws ec2 delete-tags --resource $instance_id --tags Key='ExpirationDate' --region $region --profile $PROFILE
+			aws ec2 delete-tags --resource "$instance_id" --tags Key='ExpirationDate' --region "$region" --profile "$PROFILE"
 			#Calculating the new expiration date
-			new_exp_date=$(date -d @$(($current_time+86400*$DAYS_TO_UPDATE)) +%Y-%m-%d)
+			new_exp_date=$(date -d @$((current_time+86400*DAYS_TO_UPDATE)) +%Y-%m-%d)
 			echo -e "Updating the ${BROWN}ExpirationDate${NC} tag of instance with id: ${GREEN}$instance_id${NC}, with name: ${GREEN}$instance_name${NC} in region ${BLUE}$region${NC} to ${RED}$new_exp_date${NC}"
 			#Updating the ExpirationDate tag
-			aws ec2 create-tags --resource $instance_id --tags Key=ExpirationDate,Value=$new_exp_date --region $region --profile $PROFILE
+			aws ec2 create-tags --resource "$instance_id" --tags Key=ExpirationDate,Value="$new_exp_date" --region "$region" --profile "$PROFILE"
 		fi
 	done
 done
